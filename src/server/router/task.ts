a80f590671e92502992a8bff97e380bdf131a12c
Jeremy <jeremy@je12emy.com>
2022-10-19 22:15:21 -0600

    feat: add create task function

--- src/server/router/task.ts
+++ src/server/router/task.ts
@@ -1,11 +1,27 @@
+import { z } from "zod";
 import { createProtectedRouter } from "./context";
 
-export const taskRouter = createProtectedRouter().query("all", {
-  resolve({ ctx }) {
-    return ctx.prisma.task.findMany({
-      where: {
-        userId: ctx.session.user.id,
-      },
-    });
-  },
-});
+export const taskRouter = createProtectedRouter()
+  .query("all", {
+    resolve({ ctx }) {
+      return ctx.prisma.task.findMany({
+        where: {
+          userId: ctx.session.user.id,
+        },
+      });
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      text: z.string().min(1).max(40),
+    }),
+    async resolve({ input, ctx }) {
+      const task = await ctx.prisma.task.create({
+        data: {
+          text: input.text,
+          userId: ctx.session.user.id,
+        },
+      });
+      return task;
+    },
+  });
